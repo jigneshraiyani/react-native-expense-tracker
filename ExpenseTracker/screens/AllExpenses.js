ad53daeaed77a097f98e763bdfa731d2b6368139
Jignesh Raiyani <jignesh.raiyani@Jigneshs-MacBook-Air.local>
2025-01-29 09:29:00 +0530

    Added initial project template with bootom tab

---
+++ ExpenseTracker/screens/AllExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+function AllExpenses() {
+  return <Text>AllExpense Screen</Text>;
+}
+
+export default AllExpenses;
---
+++ ExpenseTracker/screens/RecentExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+function RecentExpenses() {
+  return <Text>RecentExpensesExpense Screen</Text>;
+}
+
+export default RecentExpenses;
